3e5c0af9d9049c34bd31f827e554115ccced6602
R. Portland <raph_doppler@hotmail.com>
2012-11-19 20:55:34 +0100

    top bar
    il manque encore l'affichage du nom dans la top bar qui nécessite
    d'autres dev

--- application/views/candidat/elmt/private-head-nav.php
+++ application/views/candidat/elmt/private-head-nav.php
@@ -2,18 +2,37 @@
       <div class="navbar-inner">
           <div class='container-fluid'>
                 <?php echo anchor('fleurjob',' florBooks','class="brand"'); ?>
-                <ul class="nav pull-left">
-                  
-                  <li><?php echo anchor('fleurjob', '<i class="icon-home icon-white"></i> Accueil'); ?></li>
-                  <li><?php echo anchor('fleurjob/welcome','Mon espace'); ?></a></li>                  
-
+                <ul class="nav pull-left">    
+                    <li><a>FR</a></li>                           
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Books <b class="caret"></b></a>                        
+                        <ul class="dropdown-menu">
+                            <li><?php echo anchor("book",'<i class="icon icon-eye-open"></i> Voir'); ?></li>
+                            <li><?php echo anchor("book/my_books",'<i class="icon icon-camera"></i> Créer','class=""'); ?></li>
+                            <li><?php echo anchor("book/advice",'<i class="icon icon-question-sign"></i> Conseils','class=""'); ?></li>                          
+                        </ul>
+                     </li>
+                     <li><?php echo anchor("fleurjob/welcome",'Mon Espace'); ?></li>
+                                
                 </ul>  
                 
+                   <?php echo anchor('', 'Achat/Vente magasin','class="pull-right btn btn-info"'); ?>                                       
+                   <?php echo anchor('', 'Accès Emploi/Recrutement','class="pull-right btn btn-primary"'); ?>   
+                   
                 <ul class="nav pull-right">
-                    <li>
-                    <?php echo anchor("auth/logout","déconnexion"); ?>
-                    </li>                 
-                </ul>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">UserName <b class="caret"></b></a>                        
+                        <ul class="dropdown-menu">
+                            <li><?php echo anchor("fleurjob/edit_profile",'<i class="icon icon-user"></i> Mon profil'); ?></li>
+                            <li><?php echo anchor("auth/logout",'<i class="icon icon-off"></i> Déconnexion','class=""'); ?></li>                          
+                        </ul>
+                     </li>
+                </ul>                   
+                                        
+                    
+
+                                
+                                  
         </div>
       </div>
     </div>
